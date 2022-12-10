82ebd39fc7043d57bbfad5ed482a053925826ab4
Oleg Bukovskiy <olbukovskiy@gmail.com>
2022-12-10 21:56:49 +0200

    refactored to hooks

--- src/components/AddContact/AddContact.js
+++ src/components/AddContact/AddContact.js
@@ -4,56 +4,6 @@ import { Formik } from 'formik';
 
 import { FormInput, Label, Button, FormField } from './AddContact.styled';
 
-// export default class AddContact extends Component {
-//   state = {
-//     name: '',
-//     number: '',
-//   };
-
-//   handleChange = e => {
-//     const { name, value } = e.target;
-//     this.setState({ [name]: value });
-//   };
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-//     this.props.onSubmit(this.state.name, this.state.number);
-//     e.currentTarget.reset();
-//   };
-
-//   render() {
-//     return (
-//       <Form onSubmit={this.handleSubmit}>
-//         <Label>
-//           Name
-//           <Input
-//             type="text"
-//             name="name"
-//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//             required
-//             onChange={this.handleChange}
-//             placeholder="Enter name"
-//           />
-//         </Label>
-//         <Label>
-//           Number
-//           <Input
-//             type="tel"
-//             name="number"
-//             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//             required
-//             placeholder="Enter number"
-//             onChange={this.handleChange}
-//           />
-//         </Label>
-//         <Button type="submit"> Add new contact</Button>
-//       </Form>
-//     );
-//   }
-// }
-
 const initialValues = {
   name: '',
   number: '',
